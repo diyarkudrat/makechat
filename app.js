4a8075f233da2d859389fa6b99990a6fc38a493e
Diyar Kudrat <dkudrat@gmail.com>
2020-12-02 16:11:12 -0800

    implemented socket.io

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const app = express();
 
 const server = require('http').Server(app);
 
+//Web Socket connection
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    console.log("New user connected!")
+});
+
 const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+    extname: '.handlebars',
+    defaultLayout: null
+}));
 app.set('view engine', 'handlebars');
+
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.handlebars');
